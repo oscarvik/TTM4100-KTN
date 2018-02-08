999e0dd6709a9d26d9896fcc722d0ecae11afa54
Oscar Carl Vik <OscarVik@Oscars-MacBook-Pro.local>
2018-02-08 01:15:34 +0100

    ..

--- Øving4-UDP Pinger/UDPPingerClient.py
+++ Øving4-UDP Pinger/UDPPingerClient.py
@@ -1,17 +1,14 @@
-# This skeleton is valid for both Python 2.7 and Python 3.
-# You should be aware of your additional code for compatibility of the Python version of your choice.
-
 import time
 from socket import *
 
-# Get the server hostname and port as command line arguments                    
-host = "127.0.0.1"
+#Server hostname and port as command line arguments
+host = "127.0.0.1" #Localhost
 port = 12000
  
 # Create UDP client socket
 clientSocket = socket(AF_INET, SOCK_DGRAM)  # Note the second parameter is NOT SOCK_STREAM but the corresponding to UDP
 
-clientSocket.settimeout(1.0)  # Set socket timeout as 1 second
+clientSocket.settimeout(1.0)  # Socket timeout as 1 second
 
 ptime = 0  # Sequence number of the ping message
 
